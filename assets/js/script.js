0f67fe0f7abb78ce18cbfee8faa80ba352bd2a49
tharunoptimus <tharunoptimus@outlook.com>
2022-01-13 23:30:20 +0530

    Added global elements and defined the render function

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(() => {
+    let render = new URLSearchParams(window.location.search).get("code")
+    let defaultCode = '# To test if the Interpreter is working correctly, feel free to use this file\n# as a playground.\n# We Print MEOW to the console.\n\n# Moving Pointer to 77\nfor 77 move pointer right\n\n# Setting Memory as Pointer + 2\nmemory = pointer + 2\n\nprint char pointer # Will Print M\n\n# Moving pointer left 8 to Point 69\nmove pointer left 8\n\nprint char pointer # Will Print E\nprint char memory # Will Print O\n\n# Setting Memory to 87\nmemory = 87\nif memory == 87 print char memory'
+    
+    if (render != null) {
+        let codeToRender = decodeURI(render)
+        codeTextArea.value = codeToRender
+    } else {
+        codeTextArea.value = defaultCode
+    }
+    
+    render = render != null ? render : defaultCode
+    
+    $("#code").linedtextarea();
+})
+
+const run = document.getElementById("run")
+const share = document.getElementById("share")
+
+const codeTextArea = document.getElementById("code")
+const outputTextArea = document.getElementById("output")
+})
